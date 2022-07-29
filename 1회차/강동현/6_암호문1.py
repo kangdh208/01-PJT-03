89c6b545118275867aa5ee5ce2961acab15958ef
kangdh208 <kangdh208@gmail.com>
2022-07-29 17:11:40 +0900

    0729 PJT Finished

--- 1회차/강동현/6_암호문1.py
+++ 1회차/강동현/6_암호문1.py
@@ -6,21 +6,19 @@ for z in range(10):
     A = int(input())
     B = list(map(int,input().split()))
     C = int(input())
-    D = list(input().split('I '))
-    D = D[1:]
+    D = list(input().split('I ')) #I로 쪼개자
+    D = D[1:] # 맨앞 잔챙이 치우기
     for i in range(len(D)): # 명령어 쪼개서 리스트화
         element = list(map(int, D[i].split()))
         NewD = []
         NewD.append(element)
-
-        for i in range(C):
-            x = NewD[0][0]
-            y = NewD[0][1]
-            s = NewD[0][2:] # 이게 문제같은데
-            for j in range(y):
-                B.insert(x+j, s[j]) #insert 이렇게쓰면안되나?
+        idx = NewD[0][0] # 언제부터 바꿀꺼야
+        if NewD[0][0]: #중간이면 B를 자르고
+            B = B[:idx] + NewD[0][2 : 3+C] + B[idx:]
+        else: # 처음부터?? 그럼 B를 뒤로
+            B = NewD[0][2 : 3+C] + B
     password = []
-    for i in range(10):
+    for i in range(10): #리스트를 문자열로
         password.append(str(B[i]))
-    password = ' '.join(password)
-    print(f'#{z+1} {password}') # 
+    answer = ' '.join(password)
+    print(f'#{z+1} {answer}')
